eb3e55c7908900d96f495cbbff1db4961ef8acd5
Bibek Basak <82058568+Bibek-Basak@users.noreply.github.com>
2025-04-19 12:00:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  //treat all JS code as newer version.
+
+//alert(3+3)  //it will throw error because we are using node not browser.
+
+//console.log("123")  console.log("Bibek"); //error
+//console.log("123");  console.log("Bibek")  //works fine but reduces readability of code.not a good practice
+
+//for documentation use mdn or tc39 websites.
+
+let name = "Bibek"
+let age = 24
+let isLoggedIn = false
+let state = null
+let user;
+
+//number => 2 to power 53
+//bigInt => greater than number
+//boolean ==> returns true or false
+//string => can be used within '' or ""
+//null => standalone value, represents empty value 
+//undefined => when we do not assign any value
+//symbol => to find uniqueness when we read react
+
+//object
+
+console.log(typeof "hitesh");  //string
+console.log(typeof age)  //number
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+
+
+
+
